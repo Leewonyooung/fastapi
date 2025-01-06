699ef10e4a1d137411f2c04d3b71fc6f12636071
Leewonyoung <wylee99@naver.com>
2025-01-07 01:19:51 +0900

    fff

--- pet.py
+++ pet.py
@@ -187,7 +187,6 @@ async def update_pet(
 # 반려동물 삭제
 @router.delete("/delete/{pet_id}")
 async def delete_pet(pet_id: str):
-    redis_client = await hosts.get_redis_connection()
     conn = hosts.connect()
     try:
         with conn.cursor() as cursor:
@@ -198,8 +197,6 @@ async def delete_pet(pet_id: str):
             if result == 0:
                 raise HTTPException(status_code=404, detail="Pet not found.")
 
-            cache_key = generate_cache_key("get_pets")
-            await redis_client.delete(cache_key)
 
             return {"message": "Pet deleted successfully!"}
     finally:
